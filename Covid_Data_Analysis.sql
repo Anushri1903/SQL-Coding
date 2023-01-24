42058bab76a171ac7417165a969f84b14e790f6a
Anushri Namdeorao More <40488560+Anushri1903@users.noreply.github.com>
2023-01-23 23:09:34 -0500

    Update Covid_Data_Analysis.sql

--- Covid_Data_Analysis.sql
+++ Covid_Data_Analysis.sql
@@ -12,22 +12,21 @@ select
 location, date, total_cases, new_cases, total_deaths, population  
 from CovidDeath order by 1,2;
 
+-- Aggregating total_cases and total_deaths for INDIA --
+
 select 
-location,sum(total_cases) as sum_t_c, sum(total_deaths) as sum_t_d
+location,sum(total_cases) as sum_total_cases, sum(total_deaths) as sum_total_deaths
 from CovidDeath
+where location like '%India%'
 group by location;
 
---- Percentage Likelihood of dying if you contract COVID in your Country
+--- Percentage Likelihood of dying if you contract COVID in your Country(India)--
 select 
 location, total_cases,total_deaths, (total_deaths/total_cases)*100 as percentage
 from CovidDeath;
+where location like '%India%';
 
 ---Analysis for India only---
-
-select 
-location,date,population, total_cases,total_deaths, (total_deaths/population)*100 as percentage
-from CovidDeath
-where location like '%India%';
 
 --Looking total death VS total country population 
 select 
@@ -40,3 +39,18 @@ Select Location, MAX(Total_deaths) as TotalDeathCount
 From CovidDeath
 Where continent is not null 
 Group by Location
+
+--- Let's analyse further by Continent -- 
+-- Showing contintents with the highest death count per population
+select continent, population,max(total_deaths) as max_c_deaths
+from CovidDeath
+where continent is not null
+group by continent,population
+order by max_c_deaths desc;
+
+select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+From CovidDeath dea
+join CovidVaccination vac
+On dea.location = vac.location
+where dea.continent is not null 
+order by 2,3;
