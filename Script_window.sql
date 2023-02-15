c808c89b16857ba0747180538c84ee98fea6fa38
Anushri Namdeorao More <40488560+Anushri1903@users.noreply.github.com>
2023-02-14 20:13:20 -0500

    Update Script_window.sql

--- Script_window.sql
+++ Script_window.sql
@@ -26,3 +26,14 @@ select
 	ntile(10) over (order by customer_loyalty_score desc) as loyalty_cat2
 from grocery_db.loyalty_scores;
 
+-- temp query view  with ---
+WITH TOPTWO AS (
+    SELECT customer_id,product_area_id, sum(sales_cost) as total_spend, ROW_NUMBER() 
+    over (
+        PARTITION BY customer_id,product_area_id
+        order by sum(sales_cost)
+    ) AS RowNo 
+    FROM  grocery_db.transactions
+    group by customer_id,product_area_id
+   )
+   SELECT * FROM TOPTWO WHERE RowNo <= 2;
